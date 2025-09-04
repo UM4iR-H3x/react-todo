f7ee37001c6c95ee2df3be9ddb131874a2640066
Umair Anwari <umair.anwari789@gmail.com>
2025-09-03 22:35:45 -0700

    added the local storage

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Header from './componets/header';
 import Todo from './componets/todo';
 import Footer from './componets/footer';
 import AddToTodoList from './componets/addtodolist';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 function App() {
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }   
   // Use a local variable name (not "Todo" which is an imported binding)
-  const [todos, setTodos] = useState([
-    { id: 1, title: "Go to the market", desc: "You need to go to the market to buy stuff" },
-    { id: 2, title: "Go to the mall", desc: "You need to go to the mall to buy stuff" },
-    { id: 3, title: "Go to the school", desc: "You need to go to the school to buy stuff" },
-  ]);
+  const [todos, setTodos] = useState(initTodo);
   const addTodo = (title, desc) => {
     console.log("I am adding this todo", title, desc);
     let id;
@@ -28,12 +31,19 @@ function App() {
     }
     setTodos([...todos, myTodo]);
     console.log(myTodo);
+
   } 
 
   const onDelete = (todo) => {
     // placeholder delete handler
     setTodos(todos.filter((e) => e !== todo));
+    setTodos(todos.filter((e) => e !== todo));
+    console.log("Deleted", todo);
+
   };
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <>
